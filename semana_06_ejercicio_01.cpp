3e03aedfe5482ebb713be7ba58d0e9a5ab97083d
LaVolpeR <38415473+LaVolpeR@users.noreply.github.com>
2018-09-26 16:12:03 -0500

    Update semana_06_ejercicio_01.cpp

--- semana_06_ejercicio_01.cpp
+++ semana_06_ejercicio_01.cpp
@@ -5,80 +5,83 @@
 #include<stdio.h>
 
 using namespace std;
-float time = 0;
+
+float total = 0;
+
 struct datos{
 	float dato;
-	float 
 	string name;
+	float espera;
 	datos *sig;
 }*p,*aux,*u;
-void insertar(float dat);
+void insertar(float dat, string nam, float esp);
 void listar();
 
 int main(){
-	string name;
-	float tiempo;
-	int opc, number,i = 0;
-	cout<<"Ingrese la cantidad de usuarios que estan en el banco"<<endl;
-	cin>>number;
-	do{
-	cout<<"INGRESE NOMBRE: ";cin>>name;
-	nombre(name);
-	do{
+	int numero,opc;
+	float tiempo = 0; 
+	float ocup = 0;
+	string nombre;
+	cout<<"\t\t\tBienvenido";
+	cout<<"\n\n\t\tIngrese la cantidad de usuarios que hay en el banco"<<endl;
+	cin>>numero;
+	
+	for(int i = 0; i<numero; i++){
 		system("cls");
+		ocup+=tiempo;
 		tiempo = 0;
-	cout<<"BIENVENIDO "<<name<<endl;
-	cout<<"MENU"<<endl;
-	cout<<"1.- Retiro"<<endl;
-	cout<<"2.- Deposito"<<endl;
-	cout<<"3.- Consulta"<<endl;
-	cout<<"4.- Actualizacion"<<endl;
-	cout<<"5.- Pagos"<<endl;
-	cout<<"6.- Salir"<<endl;
-	cout<<"Elegir transaccion que desa realizar ";cin>>opc;
-	switch(opc){
-	case 1:
-		tiempo = 4;
-		insertar(tiempo);
-		cout<<"-->> Ha pasado "<<tiempo<<"Min";
-		getch();
-		break;
-	case 2:
-		tiempo = 2;
-		insertar(tiempo);
-		cout<<"-->> Ha pasado "<<tiempo<<"Min";
-		getch();
-		break;
-	case 3:
-		tiempo = 3.5;
-		insertar(tiempo);
-		cout<<"-->> Ha pasado "<<tiempo<<"Min";
-		getch();
-		break;
-	case 4:
-		tiempo = 5;
-		insertar(tiempo);
-		cout<<"-->> Ha pasado "<<tiempo<<"Min";
-		getch();
-		break;
-	case 5:
-		tiempo = 2;
-		insertar(tiempo);
-		cout<<"-->> Ha pasado "<<tiempo<<"Min";
-		getch();
-		break;
-	case 6:
-		listar();
-	cout<<"Usted estuvo: "<<time<<" min.";
-		exit(1);
-	default: cout<<"\n Opcion no valida!!";break;
-
+		
+		cout<<"\n\t\tIngrese el nombre de la "<<i+1<<" persona:"<<endl;
+		cin>>nombre;
+		do{
+			system("cls");
+			cout<<"BIENVENIDO "<<nombre<<endl;
+			cout<<"MENU"<<endl;
+			cout<<"1.- Retiro"<<endl;
+			cout<<"2.- Deposito"<<endl;
+			cout<<"3.- Consulta"<<endl;
+			cout<<"4.- Actualizacion"<<endl;
+			cout<<"5.- Pagos"<<endl;
+			cout<<"6.- Salir"<<endl;
+			cout<<"Elegir transaccion que desa realizar ";cin>>opc;	
+			switch(opc){
+				case 1:
+					tiempo += 4;
+					cout<<"-->>Ha pasado 4 minutos";
+					getch();
+					break;
+				case 2:
+					tiempo += 2;
+					cout<<"-->>Ha pasado 2 minutos";
+					getch();
+					break;
+				case 3:
+					tiempo += 3.5;
+					cout<<"-->>Ha pasado 3.5 minutos";
+					getch();
+					break;
+				case 4:
+					tiempo += 5;
+					cout<<"-->>Ha pasado 5 minutos";
+					getch();
+					break;
+				case 5:
+					tiempo += 2;
+					cout<<"-->>Ha pasado 2 minutos";
+					getch();
+					break;	
+				case 6:
+					cout<<"Usted estuvo "<<tiempo<<" minutos";
+					insertar(tiempo,nombre,ocup);
+					getch();
+				break;
+			}			
+		}while(opc!=6);
 	}
-}while(opc);
-i++;
-}while(i!=number);
+	system("cls");
+	listar();
+	cout<<"\n\nEl cajero estuvo operando "<<total<<"min.";
 }
-
 void listar(){
 	int i;
 	if(!u){
@@ -87,27 +90,20 @@ void listar(){
 	}
 	aux=p;
 	while(aux){
-		time += aux->dato;
+		if(aux->espera!=0)
+		cout<<"La persona "<<aux->name<<" estuvo esperando "<<aux->espera<<"min y sus transacciones duraron un total de "<<aux->dato<<"min."<<endl;
+		else{
+			cout<<"La persona "<<aux->name<<" fue la primera en ser atendida y demoro "<<aux->dato<<"min."<<endl;
+		}
+		total += aux->dato;
 		aux=aux->sig;
 	}
 }
-
-void insertar(float dat){
+void insertar(float dat, string nam, float esp){
 	aux=new(datos);
 	aux->dato=dat;
-	if(u){
-		u->sig=aux;
-		aux->sig=NULL;
-		u = aux;
-	}
-	else{
-		p=u=aux;
-	}
-}
-
-void nombre(string name){
-	aux=new(datos);
-	aux->name=dat;
+	aux->name = nam;
+	aux->espera =esp;
 	if(u){
 		u->sig=aux;
 		aux->sig=NULL;
